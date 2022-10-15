fccf0dc242c90d45ec31cd1e7518d2ff7cf2bd8e
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-15 11:31:51 -0700

    Added jQuery on document ready for api display

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+$(document).ready(() => {
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://weatherapi-com.p.rapidapi.com/forecast.json?q=Seattle&days=5&dt=${new Date()}`,
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "f815fbac89msh4d641516a677c7fp1cd9a8jsnd3017f41442f",
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response.forecast.forecastday[0]);
+        $('div.forecast').html(
+            `<div class='forecastday'>
+            
+            </div>
+            <div class='forecastday'></div>
+            <div class='forecastday'></div>`
+            )
+        console.log($('div.forecast').html())
+        })
+    });
+
 const searchButton = document.getElementById('search-addon');
 const searchInput = document.getElementById('search-input');
 searchButton.addEventListener('click', () => {
